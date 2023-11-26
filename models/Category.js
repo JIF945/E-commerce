a7eab3a8c19f242e650451f5f737ca796d758c45
James <joshua.james918@gmail.com>
2023-11-26 13:22:29 -0600

    category defined columns

--- models/Category.js
+++ models/Category.js
@@ -1,12 +1,22 @@
 const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require("../Develop/config/connection.js");
+const sequelize = require("../config/connection.js");
 
 class Category extends Model {}
 
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allownull: false,
+      primaryKey: true,
+      AutoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allownull: false,
+    },
   },
   {
     sequelize,
